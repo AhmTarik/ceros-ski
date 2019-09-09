f32b4812b5ce18ebd2d1c303bd2e6255eeacb2f8
a.tarik1-at-230775138934 <ahmedbadawy491@gmail.com>
2019-09-09 14:51:51 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import '../css/game.css';
+import { Game } from './Core/Game.js';
+
+document.addEventListener("DOMContentLoaded",() => {
+    const skiGame = new Game();
+    skiGame.load().then(() => {
+        console.log('run ...')
+        skiGame.init();
+        skiGame.run();
+    });
+});
